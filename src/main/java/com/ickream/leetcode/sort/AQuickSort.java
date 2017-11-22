40d3f3fd70f049dae4ffd4a32cc87cd1e8070f78
ickream <xf.meng@foxmail.com>
2017-11-22 21:29:17 +0800

    快速排序，leetcode 19链表移除倒数第n个节点

---
+++ src/main/java/com/ickream/leetcode/sort/AQuickSort.java
@@ -0,0 +1,41 @@
+package com.ickream.leetcode.sort;
+
+import java.util.Arrays;
+
+/**
+ * Created by xf on 2017/11/22.
+ */
+public class AQuickSort {
+
+    private static void sort(int [] arr,int low,int high){
+        if(low>=high){
+            return;
+        }
+        int lo=low++;
+        int hi=high;
+        while(low<=high){
+            if(arr[low]<=arr[lo]){
+                low++;
+                continue;
+            }
+            if(arr[high]>=arr[lo]){
+                high--;
+                continue;
+            }
+            change(arr,low,high);
+        }
+        change(arr, lo, high);
+        sort(arr, lo, high-1);
+        sort(arr, high+1,hi);
+    }
+    private static void change(int [] arr, int i, int j){
+        int tmp=arr[i];
+        arr[i]=arr[j];
+        arr[j]=tmp;
+    }
+    public static void main(String [] args){
+        int[] arr=new int []{3,1,2,3,4,5};
+        sort(arr, 0, arr.length-1);
+        System.out.println(Arrays.toString(arr));
+    }
+}
