66a1c2a129a0950c9a22ad6c2563d1ae193ad1f1
ickream <xf.meng@foxmail.com>
2017-12-05 09:26:02 +0800

    num331验证字符串是否为二叉树的前序遍历，stack

---
+++ src/main/java/com/ickream/leetcode/stack/Num331IsValidSerialization.java
@@ -0,0 +1,34 @@
+package com.ickream.leetcode.stack;
+
+import java.util.LinkedList;
+import java.util.Stack;
+
+/**
+ * Created by xf on 2017/12/1.
+ *  验证字符串是否是二叉树的前序遍历
+ */
+
+public class Num331IsValidSerialization {
+    public static boolean isValidSerialization(String preorder) {
+        if(preorder.equals("")){
+            return false;
+        }
+        String [] strings = preorder.split(",");
+        Stack<String> stack= new Stack<>();
+        for(String cur : strings){
+            while(cur.equals("#") && !stack.isEmpty() && stack.peek().equals("#")){
+                stack.pop();
+                if(stack.isEmpty()){
+                    return false;
+                }
+                stack.pop();
+            }
+            stack.push(cur);
+        }
+        return stack.size() == 1 && stack.peek().equals("#");
+    }
+    public static void main(String [] args){
+        isValidSerialization("1,#,#");
+
+    }
+}
